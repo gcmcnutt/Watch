e2c76a74523e4589da2f2ad0f22c1b2cfc870abb
Greg McNutt <greg.mcnutt@corp.vendio.com>
2015-05-31 14:17:30 -0700

    Initial Commit

---
+++ Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watch
+//
+//  Created by Greg McNutt on 5/31/15.
+//  Copyright (c) 2015 Greg McNutt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
